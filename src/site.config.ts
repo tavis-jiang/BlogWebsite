41f7da674bfd8e5d0289adc8105794a5cdf3860c
tavis_jiang <913669023@qq.com>
2026-04-27 21:50:37 +0800

    update

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,9 +3,9 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Astro Theme Pure',
+  title: 'Tavis\ Island',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'Tavis',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Stay hungry, stay foolish',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
@@ -79,7 +79,7 @@ export const theme: ThemeUserConfig = {
     credits: true,
     /** Optional details about the social media accounts for this site. */
     social: [
-      { icon: 'github', label: 'GitHub', href: 'https://github.com/cworld1/astro-theme-pure' },
+      { icon: 'github', label: 'GitHub', href: 'https://github.com/tavis-jiang' },
       { icon: 'rss', label: 'RSS', href: '/rss.xml' }
     ]
   },
